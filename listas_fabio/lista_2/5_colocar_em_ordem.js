15d371f9aaf15546f1aaadfddc8fc7570ac9678d
Nicolas Franklin <55466904+nicolasfrankli@users.noreply.github.com>
2022-04-21 12:24:36 -0300

    Update and rename 5.js to 5_colocar_em_ordem.js

--- listas_fabio/lista_2/5_colocar_em_ordem.js
+++ listas_fabio/lista_2/5_colocar_em_ordem.js
@@ -1,10 +1,10 @@
 import { input } from '../../io_utils.js'
 function main(){
     const [n1, n2, n3] = input('Type three numbers:').split(' ').map(Number)
-    console.log(n_maior(n1, n2, n3))
+    console.log(ordem(n1, n2, n3))
 }
 
-function n_maior(a, b, c){
+function ordem(a, b, c){
     if(a <= b && b <= c){
         return [a, b, c]
     } else if(a <= c && c <= b){
