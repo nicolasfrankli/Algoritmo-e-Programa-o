3191f2f6b20de386be059802dafcd9aa27267b63
Nicolas Franklin <55466904+nicolasfrankli@users.noreply.github.com>
2022-06-07 17:33:28 -0300

    Update 9_multiples_of_13.js

--- beecrowd/3_for_and_while/9_multiples_of_13.js
+++ beecrowd/3_for_and_while/9_multiples_of_13.js
@@ -3,18 +3,31 @@ const input = prompt()
 
 function main(){
 
-    let initial_value = Number(input())
-    let final_value = Number(input())
-    const [first_term, final_term] = arithmetic_progression_interval(initial_value, final_value, 13)
-    const sum_of_multiples_of_13 = sum_of_terms_of_arithmetic_progression(first_term, final_term, 13)
+    let n1 = Number(input())
+    let n2 = Number(input())
+    let[initial_value, final_value] = order(n1, n2)
+    const [first_term_pa_13_multiples, final_term_pa_13_multiples] = arithmetic_progression_interval(initial_value, final_value, 13)
+    const sum_of_multiples_of_13 = sum_of_terms_of_arithmetic_progression(first_term_pa_13_multiples, final_term_pa_13_multiples, 13)
     const total_sum = sum_of_terms_of_arithmetic_progression(initial_value, final_value, 1)
     console.log(total_sum - sum_of_multiples_of_13)
 
 }
 
+function order(number1, number2){
+
+    if(number1 < number2){
+        
+        return [number1, number2]
+    
+    } else {
+        
+        return [number2, number1]
+    
+    }
+}
 
 function is_multiple(number, divider){
-    if(number%divider){
+    if(number%divider === 0){
         return true
     }
 }
@@ -25,23 +38,38 @@ function arithmetic_progression_interval(initial_value, final_value, reason){
     let final_term_pa = final_value
     
     while (!is_multiple(first_term_pa, reason)){
+        
         first_term_pa++
+    
     }
 
     while (!is_multiple(final_term_pa, reason)){
+        
         final_term_pa--
+    
     }
 
+    if(first_term_pa === final_term_pa){
+        
+        final_term_pa = 0
+    
+    }
+    
     return [first_term_pa, final_term_pa]
 
 }
 
 function sum_of_terms_of_arithmetic_progression(first_term, final_term, reason){
 
-    const number_of_terms = (final_term - first_term)/reason + 1
-    return (first_term + final_term)*number_of_terms/2
+    if(final_term !== 0){
 
+        let number_of_terms = (final_term - first_term)/reason + 1
+        return ((first_term + final_term)*number_of_terms)/2
 
+    } else{
+
+        return first_term
+    }
 }
 
 
