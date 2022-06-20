db285ffbf6c8f9424174cf021b546c1fcc9da760
Nicolas Franklin <55466904+nicolasfrankli@users.noreply.github.com>
2022-06-20 17:14:22 -0300

    Create 7_dollar.js

---
+++ atividades_nicolas/g1/prova/7_dollar.js
@@ -0,0 +1,17 @@
+import prompt from 'prompt-sync';
+const input = prompt();
+
+function main(){
+    
+    const current_dollar_exchange_rate = input('Dollar exchange rate on June 20: U$')
+    const quantity_of_dollars_to_buy = input('How many dollars do you want to buy?')
+    
+    const value = current_dollar_exchange_rate * quantity_of_dollars_to_buy
+    console.log(`IOF: R$${(value * 0.011).toFixed(2)}
+You must pay ${(value * 1.011).toFixed(2)}`)
+
+
+}
+
+
+main()
