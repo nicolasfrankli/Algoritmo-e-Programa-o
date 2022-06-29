8873ffa5978bc62c05a63df92a567701bb176a41
Nicolas Franklin <55466904+nicolasfrankli@users.noreply.github.com>
2022-06-29 07:56:37 -0300

    Update 8_loan.js

--- atividades_nicolas/g1/prova/8_loan.js
+++ atividades_nicolas/g1/prova/8_loan.js
@@ -8,26 +8,30 @@ function main(){
     if (loan_term < 2 || loan_term > 24){
         console.log('The loan term must be bigger than 2 and lowest than 24')
     } else {
-        const iof = (0.0038 * loan_amount) + (0.000082 * 30 * loan_term)
-        const interest = (monthly_income + iof)
+        const iof = loan_amount * (0.0038 + (0.000082 * 30 * loan_term))
+        let AmountofInterestPayable = loan_amount / loan_term
         const selic = 0.1275
         
         if(loan_term <= 6){
-            taxes = 0.5 * selic
+            AmountofInterestPayable *= 0.5 * selic
         } else if(loan_term <= 12){
-            taxes = 0.75 * selic
+            AmountofInterestPayable *= 0.75 * selic
         } else if (loan_term <= 18){
-            taxes = selic
+            AmountofInterestPayable *= selic
         } else {
-            taxes = 1.38 * selic
+            AmountofInterestPayable *= 1.38 * selic
         }
+        console.log(`IOF Value: R$${iof.toFixed(2)}
+Amount of Interest Payable: R$${(AmountofInterestPayable).toFixed(2)}
+Total Amount Payable: R$${(loan_amount + iof + AmountofInterestPayable).toFixed(2)}
+Amount of the Monthly Installment: ${loan_term}x of R$${(loan_amount / loan_term).toFixed(2)}`)
     }
 
-
-    console.log(`IOF: R$${iof.toFixed(2)}
-Interest: ${().toFixed(2)}`)
-
-
+    if (loan_amount / loan_term > 0.4 * monthly_income){
+        console.log('Loan denied!')
+    } else {
+        console.log('Aproved Loan!')
+    }
 }
 
 
